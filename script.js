4a365360f31d22b2f8cc1169f3341cd09b3ecf38
IamBara <mabdillahbarqah@gmail.com>
2021-07-23 21:13:49 +0700

    looping done

---
+++ script.js
@@ -0,0 +1,32 @@
+// // let max_user = 100
+// // for (let i = 1; i <= max_user; i++){
+// //     // console.log("user ke-",i);
+// //     document.write("user ke-", i, "<br>")
+// // }
+
+// // let max_loop = 10
+// // for(let i=0 ; i<=max_loop*2; i +=2){
+// //     console.log(i);
+// // }
+
+// // let loop=20
+// // for(let i=0; i<=loop; i++){
+// //     if(i%2 ===0){
+// //         console.log("genap");
+// //     } else {console.log("ganjil");}
+// // }
+
+// // let yesConfirm = confirm("Apakah anda mau mengulang?")
+// // let count = 0
+// // while(yesConfirm==true)
+// //     {yesConfirm= confirm("Apakah anda mau mengulang?")
+// //     if(yesConfirm==true) {count++}
+// //     else{alert("perulangan sudah dilakukan sebanyak" + count)}
+// // }
+
+// let ib= prompt("sebutkan kepanjangan dari IB");
+// let check=ib === "Impact Byte";
+// console.log(ib, check);
+//     while(!check) {ib= prompt("sebutkan kepanjangan dari IB");
+//     break;
+//     }
